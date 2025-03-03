970efdadb7b46c7f586e7870a8f6c6fc4401ed8a
jhyeong <hyeongcode@gmail.com>
2025-03-03 14:39:39 +0900

    feat: partTimer Pay API add

--- src/api/partTimerapi/PartTimerAPI.js
+++ src/api/partTimerapi/PartTimerAPI.js
@@ -65,3 +65,35 @@ export const getPartTimerWorkHistoryList = async (pno, page) => {
 
     return res.data.data;
 }
+
+//employer 별 총 급여 get
+export const getTotalPay = async () => {
+
+    const res = await axios.get(`${host}/totalPay/${eno}`);
+
+    return res.data.data;
+}
+
+//급여(연, 월 선택)
+export const getPayByYearMonth = async (year, month) => {
+
+    const res = await axios.get(`${host}/payByYearMonth/${eno}?year=${year}&month=${month}`);
+
+    return res.data.data;
+}
+
+//급여(연도만 선택)
+export const getPayByYear = async (year) => {
+
+    const res = await axios.get(`${host}/payByYear/${eno}?year=${year}`);
+
+    return res.data.data;
+}
+
+//급여 포함한 근로자 리스트
+export const getPartTimerListWithPay = async (year, month) => {
+
+    const res = await axios.get(`${host}/listWithPay/${eno}?year=${year}&month=${month}`);
+
+    return res.data.data;
+}
