7260bff9d66dc4ee283e94d8c6e25b4c831b1692
EonEon2 <duqdjs123@naver.com>
2025-03-14 14:03:37 +0900

    chore: logo edit

--- src/pages/employerpages/EmployerIndexPage.jsx
+++ src/pages/employerpages/EmployerIndexPage.jsx
@@ -1,10 +1,13 @@
 import {Outlet} from "react-router-dom";
+import BasicLayout from "../../layout/BasicLayout.jsx";
 
 
 function EmployerIndexPage() {
     return (
         <>
+            <BasicLayout>
             <Outlet/>
+            </BasicLayout>
         </>
     )
 }
