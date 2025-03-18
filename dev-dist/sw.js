6ebe91cac05e314d3a9c5304b90cbc9d732833a3
yun9699 <cha23215@daum.net>
2025-03-18 16:53:51 +0900

    feat: jobposting layout fixed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.9je553sc4i8"
+    "revision": "0.lahsol0j2f8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
