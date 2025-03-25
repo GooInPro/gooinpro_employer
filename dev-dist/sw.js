a1b72e39eca4dcf43938ebca4cacfefa4e61f345
yun9699 <cha23215@daum.net>
2025-03-25 17:12:09 +0900

    feat: jobpositng list logic change

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.5sdo9nvf7rg"
+    "revision": "0.5lj5tak01gg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
