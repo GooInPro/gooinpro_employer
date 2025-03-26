75a945bfd77fac7199c9d3055065868861161ec6
yun9699 <cha23215@daum.net>
2025-03-27 01:20:21 +0900

    chore: ReadComponent fix Starting

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0dheujna1hg"
+    "revision": "0.scta0sh6ld"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
